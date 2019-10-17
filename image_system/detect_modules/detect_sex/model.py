90ea7c2c6dd601a592d67d8787c0e518c25bbf61
ItoMasaki <l1sum@icloud.com>
2019-10-17 22:12:54 +0900

    change machine learning model

--- image_system/detect_modules/detect_sex/model.py
+++ image_system/detect_modules/detect_sex/model.py
@@ -1,74 +1,161 @@
-from chainer import Chain
-from chainer import links as L
-from chainer import functions as F
-
-from numpy import argmax, array, uint8
-
-from matplotlib import pyplot as plt
-
-from sys import exit
-
-# channel width  height
-# 3       96     96
+# -*- coding: utf-8 -*-
+import chainer
+import chainer.links
+import chainer.functions
 
 
-
-
-class VGG_16(Chain):
+class Alex(chainer.Chain):
+    
     def __init__(self):
-        super(VGG_16, self).__init__()
-
-        with self.init_scope():
-            self.conv1  = L.Convolution2D(3, 3, 5, pad=2)
-            self.conv2  = L.Convolution2D(3, 6, 5, pad=2)
-            # maxpooling
-            self.conv3  = L.Convolution2D(6, 6, 5, pad=2)
-            self.conv4  = L.Convolution2D(6, 12, 5, pad=2)
-            # maxpooling
-            self.conv5  = L.Convolution2D(12, 12, 4, pad=2)
-            self.conv6  = L.Convolution2D(12, 12, 4, pad=2)
-            self.conv7  = L.Convolution2D(12, 24, 4, pad=2)
-            # maxpooling
-            self.conv8  = L.Convolution2D(24, 24, 3, pad=2)
-            self.conv9  = L.Convolution2D(24, 24, 3, pad=2)
-            self.conv10 = L.Convolution2D(24, 48, 3, pad=2)
-            # maxpooling
-            self.conv11 = L.Convolution2D(48, 48, 2, pad=2)
-            self.conv12 = L.Convolution2D(48, 48, 2, pad=2)
-            self.conv13 = L.Convolution2D(48, 96, 2, pad=2)
-            # maxpooling
-            self.conv14 = L.Convolution2D(96, 96, 2, pad=2)
-
-            self.line1  = L.Linear(16224, 4096)
-            self.line2  = L.Linear(4096, 4096)
-            self.line3  = L.Linear(4096, 10)
-
+        super(Alex, self).__init__(
+            conv1=chainer.links.Convolution2D(3, 48, 3, stride=1),
+            bn1=chainer.links.BatchNormalization(48),
+            conv2=chainer.links.Convolution2D(48, 128, 3, pad=1),
+            bn2=chainer.links.BatchNormalization(128),
+            conv3=chainer.links.Convolution2D(128, 192, 3, pad=1),
+            conv4=chainer.links.Convolution2D(192, 192, 3, pad=1),
+            conv5=chainer.links.Convolution2D(192, 128, 3, pad=1),
+            fc6=chainer.links.Linear(None, 1024),
+            fc7=chainer.links.Linear(None, 1024),
+            fc8=chainer.links.Linear(None, 2)
+        )
+        self.train = True
+    
     def __call__(self, x):
-        h1 = F.relu(self.conv1(x))
+        h = self.bn1(self.conv1(x))
+        h = chainer.functions.max_pooling_2d(chainer.functions.relu(h), 3, stride=2)
+        h = self.bn2(self.conv2(h))
+        h = chainer.functions.max_pooling_2d(chainer.functions.relu(h), 3, stride=2)
+        h = chainer.functions.relu(self.conv3(h))
+        h = chainer.functions.relu(self.conv4(h))
+        h = chainer.functions.relu(self.conv5(h))
+        h = chainer.functions.max_pooling_2d(chainer.functions.relu(h), 2, stride=2)
+        h = chainer.functions.dropout(chainer.functions.relu(self.fc6(h)))
+        h = chainer.functions.dropout(chainer.functions.relu(self.fc7(h)))
+        h = self.fc8(h)
+        return h
 
-        h2 = F.max_pooling_2d(F.relu(self.conv2(h1)), 2)
 
-        h3 = F.relu(self.conv3(h2))
+class Model:
+    def __init__(self):
+        self.model = chainer.links.Classifier(Alex())
+    
+    def load(self, filename):
+        chainer.serializers.load_npz(filename, self.model)
+    
+    def save(self, filename):
+        chainer.serializers.save_npz(filename, self.model)
+    
+    def predictor(self, x):
+        return self.model.predictor(x)
+    
+    def get_model(self):
+        return self.model
 
-        h4 = F.max_pooling_2d(F.relu(self.conv4(h3)), 2)
 
-        h5 = F.relu(self.conv5(h4))
-
-        h6 = F.relu(self.conv6(h5))
-        h7 = F.max_pooling_2d(F.relu(self.conv7(h6)), 2)
-
-        h8 = F.relu(self.conv8(h7))
-        h9 = F.relu(self.conv9(h8))
-        h10 = F.max_pooling_2d(F.relu(self.conv10(h9)), 2)
-
-        h11 = F.relu(self.conv11(h10))
-        h12 = F.relu(self.conv12(h11))
-        h13 = F.max_pooling_2d(F.relu(self.conv13(h12)), 2)
-
-        h14 = F.relu(self.conv14(h13))
-
-        h15 = F.relu(self.line1(h14))
-        h16 = F.relu(self.line2(h15))
-        h17 = F.relu(self.line3(h16))
-
-        return h17
+class GoogLeNet(chainer.Chain):
+    insize = 224
+    
+    def __init__(self):
+        super(GoogLeNet, self).__init__(
+            
+            conv1=chainer.links.Convolution2D(3, 64, 7, stride=2, pad=3),
+            
+            conv2_reduce=chainer.links.Convolution2D(64, 64, 1),
+            
+            conv2=chainer.links.Convolution2D(64, 192, 3, stride=1, pad=1),
+            
+            inc3a=chainer.links.Inception(192, 64, 96, 128, 16, 32, 32),
+            
+            inc3b=chainer.links.Inception(256, 128, 128, 192, 32, 96, 64),
+            
+            inc4a=chainer.links.Inception(480, 192, 96, 208, 16, 48, 64),
+            
+            inc4b=chainer.links.Inception(512, 160, 112, 224, 24, 64, 64),
+            
+            inc4c=chainer.links.Inception(512, 128, 128, 256, 24, 64, 64),
+            
+            inc4d=chainer.links.Inception(512, 112, 144, 288, 32, 64, 64),
+            
+            inc4e=chainer.links.Inception(528, 256, 160, 320, 32, 128, 128),
+            
+            inc5a=chainer.links.Inception(832, 256, 160, 320, 32, 128, 128),
+            
+            inc5b=chainer.links.Inception(832, 384, 192, 384, 48, 128, 128),
+            
+            loss3_fc=chainer.links.Linear(1024, 1000),
+            
+            loss1_conv=chainer.links.Convolution2D(512, 128, 1),
+            
+            loss1_fc1=chainer.links.Linear(2048, 1024),
+            
+            loss1_fc2=chainer.links.Linear(1024, 1000),
+            
+            loss2_conv=chainer.links.Convolution2D(528, 128, 1),
+            
+            loss2_fc1=chainer.links.Linear(2048, 1024),
+            
+            loss2_fc2=chainer.links.Linear(1024, 1000)
+        
+        )
+        
+        self.train = True
+    
+    def __call__(self, x):
+        h = chainer.functions.relu(self.conv1(x))
+        
+        h = chainer.functions.local_response_normalization(
+            
+            chainer.functions.max_pooling_2d(h, 3, stride=2), n=5, k=1, alpha=2e-05)
+        
+        h = chainer.functions.relu(self.conv2_reduce(h))
+        
+        h = chainer.functions.relu(self.conv2(h))
+        
+        h = chainer.functions.max_pooling_2d(
+            
+            chainer.functions.local_response_normalization(h, n=5, k=1, alpha=2e-05), 3, stride=2)
+        
+        h = self.inc3a(h)
+        
+        h = self.inc3b(h)
+        
+        h = chainer.functions.max_pooling_2d(h, 3, stride=2)
+        
+        h = self.inc4a(h)
+        
+        l = chainer.functions.average_pooling_2d(h, 5, stride=3)
+        
+        l = chainer.functions.relu(self.loss1_conv(l))
+        
+        l = chainer.functions.relu(self.loss1_fc1(l))
+        
+        l = self.loss1_fc2(l)
+        
+        h = self.inc4b(h)
+        
+        h = self.inc4c(h)
+        
+        h = self.inc4d(h)
+        
+        l = chainer.functions.average_pooling_2d(h, 5, stride=3)
+        
+        l = chainer.functions.relu(self.loss2_conv(l))
+        
+        l = chainer.functions.relu(self.loss2_fc1(l))
+        
+        l = self.loss2_fc2(l)
+        
+        h = self.inc4e(h)
+        
+        h = chainer.functions.max_pooling_2d(h, 3, stride=2)
+        
+        h = self.inc5a(h)
+        
+        h = self.inc5b(h)
+        
+        h = chainer.functions.average_pooling_2d(h, 7, stride=1)
+        
+        y = self.loss3_fc(chainer.functions.dropout(h, 0.4, train=self.train))
+        
+        return y
